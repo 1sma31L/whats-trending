f386f88678bdf5e89c74d3f5f4f9ea923b8035a1
ismail <ismailismo78@gmail.com>
2024-08-21 17:38:16 +0100

    chore: Update npm dependency to latest stable version

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,14 +1,37 @@
 import AnimatedDiv from "@/components/AnimatedDiv";
 import React from "react";
+import "dotenv/config";
 
-function page() {
+async function page() {
+	let movies = [];
+	const url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
+	const options = {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			Authorization: `Bearer ${process.env.ACCESS_TOKEN_AUTH}`,
+		},
+		next: { revalidate: 7200 },
+	};
+	try {
+		const response = await fetch(url, options);
+		const data = await response.json();
+		movies = data.results;
+		console.log(movies[0]);
+	} catch (err) {
+		console.error(err);
+	}
 	return (
 		<AnimatedDiv id={1} className="text-[12px] sm:text-[14px]">
 			<div>
-				movies Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Perferendis dicta iure ex quae quod eligendi corrupti excepturi itaque
-				dolorem voluptatum? Ratione, porro laboriosam reprehenderit ipsa
-				repudiandae suscipit dolor nihil magnam!
+				{movies.map((movie: any) => {
+					return (
+						<div className="py-2">
+							<h3 className="text-[20px]">{movie.title}</h3>
+							<p className="text-[15px] text-zinc-500">{movie.overview}</p>
+						</div>
+					);
+				})}
 			</div>
 		</AnimatedDiv>
 	);
