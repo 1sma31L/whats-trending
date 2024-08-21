d3eb4afe449d8466aa52da788b542eda1b4508cf
ismail <ismailismo78@gmail.com>
2024-08-21 17:47:12 +0100

    chore: Refactor movies page to use async/await for fetching movie data

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -20,8 +20,7 @@ export type TMovie = {
 	vote_count: number;
 };
 
-async function page() {
-	let movies: TMovie[] = [];
+async function getMovies() {
 	const url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
 	const options = {
 		method: "GET",
@@ -29,31 +28,40 @@ async function page() {
 			accept: "application/json",
 			Authorization: `Bearer ${process.env.ACCESS_TOKEN_AUTH}`,
 		},
-		next: { revalidate: 7200 },
 	};
+
 	try {
 		const response = await fetch(url, options);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
 		const data = await response.json();
-		movies = data.results;
-		console.log(movies[0]);
+		return data.results || [];
 	} catch (err) {
-		console.error(err);
+		console.error("Fetch error:", err);
+		return [];
 	}
+}
+
+async function Page() {
+	const movies: TMovie[] = await getMovies();
+
 	return (
 		<AnimatedDiv id={1} className="text-[12px] sm:text-[14px]">
 			<div>
-				{movies.length &&
-					movies.map((movie: any) => {
-						return (
-							<div className="py-2">
-								<h3 className="text-[20px]">{movie.title}</h3>
-								<p className="text-[15px] text-zinc-500">{movie.overview}</p>
-							</div>
-						);
-					})}
+				{movies.length > 0 ? (
+					movies.map((movie) => (
+						<div key={movie.id} className="py-2">
+							<h3 className="text-[20px]">{movie.title}</h3>
+							<p className="text-[15px] text-zinc-500">{movie.overview}</p>
+						</div>
+					))
+				) : (
+					<p>No movies available.</p>
+				)}
 			</div>
 		</AnimatedDiv>
 	);
 }
 
-export default page;
+export default Page;
