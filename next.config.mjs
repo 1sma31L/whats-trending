3cc6750375ae5612c1c29743deb1efc2419ce0ba
ismail <ismailismo78@gmail.com>
2024-08-23 13:11:47 +0100

    fix: v1.0.1 add Image Component fron next to better render and sloved line-clamp passing to another line bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "image.tmdb.org",
+				port: "",
+				pathname: "/t/p/w200/**",
+			},
+			{
+				protocol: "https",
+				hostname: "cdn.myanimelist.net",
+				port: "",
+				pathname: "/images/anime/**",
+			},
+		],
+	},
+};
 
 export default nextConfig;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Image from "next/image";
 export type TGenere = {
 	id?: number;
 	name: string;
@@ -512,28 +512,35 @@ async function Card({
 	const genres =
 		type === "movie" ? movieGenres : type === "tv" ? tvGenres : animeGenres;
 	return (
-		<div className="flex flex-row min-h-[150px] gap-1 my-4 border rounded-md md:rounded-xl  xl:hover:translate-x-2 overflow-hidden transition-all duration-300 dark:border-zinc-800">
-			<img
+		<div className="flex flex-row min-h-[150px] gap-1 my-4 border rounded-md md:rounded-xl  xl:hover:translate-x-2 overflow-hidden transition-all duration-300 dark:border-zinc-800 relative">
+			<Image
 				src={poster_path}
-				alt=""
-				className={`w-[120px] md:w-[150px] object-cover`}
+				alt="Poster Image"
+				quality={75}
+				className={`object-cover w-[120px] md:w-[150px]`}
+				width={180}
+				height={250}
+				priority
 			/>
-			<div className="p-3 r-4 order-2 overflow-hidden">
-				<h3 className="font-bold text-[20px] sm:text-[24px] font-Fragment my-1 pr-1 line-clamp-2 text-zinc-900 dark:text-zinc-100">
+
+			<div className="p-3 pr-4 flex flex-col">
+				<h3 className="font-bold text-[20px] sm:text-[24px] font-Fragment my-1 line-clamp-2 text-zinc-900 dark:text-zinc-100">
 					{`${title} (${release_date ? release_date.slice(0, 4) : ""})`}
 				</h3>
-				<p className="text-[12px] md:text-[14px] text-zinc-500 dark:text-zinc-400 py-1 indent-1 line-clamp-4 lg:line-clamp-6  max-w-full mb-3 text-ellipsis max-h-[65px] sm:max-h-[68px] md:max-h-[90px]">
-					{overview}
-				</p>
-				<div className="flex flex-row flex-wrap gap-y-1 gap-x-1 pr-1">
+				<div className="flex-grow max-w-full">
+					<p className="text-[12px] md:text-[14px] text-zinc-500 dark:text-zinc-400 line-clamp-3 md:line-clamp-4 lg:line-clamp-5  w-full text-ellipsis">
+						{overview}
+					</p>
+				</div>
+				<div className="flex flex-row flex-wrap gap-1 md:gap-2 mb-1 mt-3">
 					{genre_ids && type !== "anime"
 						? genre_ids.map((id) => {
 								const genre = genres.find((genre) => genre.id === id);
 								return (
 									<span
 										key={id}
-										className={`text-[9px] font-bold md:text-[12px] text-white
-								${genre?.color || "bg-[#2d3748]"} bg-black py-1 px-2 rounded-md`}>
+										className={`text-[10px] font-bold md:text-[12px] text-white
+								${genre?.color || "bg-[#2d3748]"} bg-black py-1 px-2  rounded-md`}>
 										{genre?.name}
 									</span>
 								);
